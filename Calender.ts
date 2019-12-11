9b39963928cbe5972c51df1b5a31d980b96a8f12
Madhusudhansharma <jonnabattla1997@gmail.com>
2019-12-11 16:55:48 +0530

    Create Calender.ts

---
+++ Calender.ts
@@ -0,0 +1,7 @@
+view: CalendarView = CalendarView.Month;
+  viewDate: Date = new Date();
+  
+  //events$: Observable<Array<CalendarEvent[]>>;
+  events$: Observable<Array<Event[]>>;
+  
+  // events: CalendarEvent[] = [];
